6ee286395331ae02f028184b15ccea3628d776d5
Vieira Neto <vieiranetoc@gmail.com>
2017-11-15 09:39:09 -0200

    trabalho finalizado

--- trabalho_02_ED.c
+++ trabalho_02_ED.c
@@ -2,12 +2,13 @@
  *Estudante de Engenharia Mecatrônica na Universidade de Brasília - UnB
  *Estrutura de Dados - Turma: C
  *professor: EDUARDO ADILIO PELINSON ALCHIERI
- *												*/
+ *TRABALHO 2								*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <time.h>
 
 typedef struct no {
 	char dado;
@@ -173,15 +174,23 @@ void arvore_function(){
 	write_arvore (raiz, arq_morse, code);
 	fclose (arq_morse);
 	
-	printf ("\nMENSAGEM CODIFICADA:\n");
+	printf ("\nMENSAGEM CODIFICADA:\n\n");
 	arq_message = fopen ("mensagem.txt", "r");
 	read_text_arvore (arq_message);
 	printf ("\n");
 	fclose (arq_message);
 
 	arq_message = fopen ("mensagem.txt", "r");
-	printf("TEXTO DECODIFICADO PELA ARVORE BINARIA: \n\n");
+	printf("\n\nTEXTO DECODIFICADO PELA ARVORE BINARIA: \n\n");
+	clock_t tempo[2];
+	tempo[0] = clock();
 	decodify_arvore (raiz, arq_message);
+	tempo[1] = clock();
+	double tempo_arvore = (tempo[1] - tempo[0]) * 1000.0 / CLOCKS_PER_SEC;
+	printf("\n\nTempo de Execussão: %lf ms.\n\n", tempo_arvore);
+	printf ("Aperte ENTER para continuar.");
+	getchar();
+	system ("clear");
 	printf ("\n\n");
 	fclose (arq_message);
 }
@@ -199,27 +208,35 @@ void lista_function(){
 	write_lista (no, arq_morse);
 	fclose (arq_morse);
 
-	printf ("\nMENSAGEM CODIFICADA:\n");
+	printf ("\nMENSAGEM CODIFICADA:\n\n");
 	arq_message = fopen ("mensagem.txt", "r");
 	read_code_lista (arq_message);
 	fclose (arq_message);
 
-	printf("\nTEXTO DECODIFICADO PELA LISTA ENCADEADA: \n\n");
+	printf("\n\nTEXTO DECODIFICADO PELA LISTA ENCADEADA: \n\n");
 	arq_message = fopen ("mensagem.txt", "r");
+	clock_t tempo[2];
+	tempo[0] = clock();
 	decodify_lista (arq_message, no);
+	tempo[1] = clock();
+	double tempo_lista = (tempo[1] - tempo[0]) * 1000.0 / CLOCKS_PER_SEC;
+	printf("\n\nTempo de Execussão: %lf ms.\n\n", tempo_lista);
+	printf ("Aperte ENTER para continuar.");
+	getchar();
+	system ("clear");
 	fclose (arq_message);
 	printf("\n\n");
 }
 
 int main (void){
 	int function;
-
+	system ("clear");
 	printf ("\nBEM VINDO AO PROGRAMA DECODIFICADOR DE CODIGO MORSE\n");
 	printf ("PARA DECOFICADAR, CRIE NA MESMA PASTA DESTE PROGRAMA \n");
 	printf ("UM ARQUIVO NOMEADO morse.txt CONTENDO O DICIONARIO MORSE\n");
 	printf ("E UM NOMEADO mensagem.txt CONTENDO A MENSAGEM A SER DECODIFICADA\n\n");
 	do{
-		printf ("ESCOLHA A FORMA COMO O PROGRAMA DECODIFICARAR SUA MENSAGEM:\n\n");
+		printf ("*ESCOLHA A FORMA COMO O PROGRAMA DECODIFICARAR SUA MENSAGEM:\n\n");
 		printf ("1) ARVORE BINARIA DE BUSCA\n\n");
 		printf ("2) LISTA ENCADEADA\n\n");
 		printf ("3) SAIR DO PROGRAMA\n\n");

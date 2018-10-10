f7da8338b525b89a3c0bb5d3329b7a32e7110ac1
Kelson André Venâncio Teixeira <txkelson@gmail.com>
2018-10-10 17:58:36 -0300

    mudanca no cabecalho

--- codigo.c
+++ codigo.c
@@ -1,7 +1,6 @@
 /*Kelson Teixeira
- *Estudante de Engenharia Mecatrônica na Universidade de Brasília - UnB
- *Estrutura de Dados - Turma: C
- *professor: EDUARDO ADILIO PELINSON ALCHIERI
+*Estudante de Engenharia Mecatrônica - UnB (Universidade de Brasília)
+*Trabalho desenvolvido para a Disciplina ED (Estrutura de Dados)*/
  *												*/
 
 #include <stdio.h>
